27bf3e0bc294f7439938a3b8544d221cde90c52f
HARIPRIYA S <sharipriya2005@gmail.com>
2026-06-23 19:39:22 +0530

    Time: 45 ms (27.14%) | Memory: 47.4 MB (26.47%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,20 +1,30 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        int n = s.length();
-        int k = p.length();
-        int[] pcount = new int[26];
+        if(s.length()<p.length()) return res;
+        HashMap<Character,Integer> pmap = new HashMap<>();
+        HashMap<Character,Integer> smap = new HashMap<>();
         for(char c : p.toCharArray()) {
-            pcount[c-'a']++;
+            pmap.put(c,pmap.getOrDefault(c,0)+1);
         }
-        for(int i=0;i<=n-k;i++) {
-            int[] scount = new int[26];
-            for(int j=i;j<=i+k-1;j++) {
-                scount[s.charAt(j)-'a']++;
-            }
-            if(Arrays.equals(pcount,scount)) {
-                res.add(i);
-            }
+        int left = 0, count = p.length();
+        for(int right=0;right<s.length();right++) {
+            char ch = s.charAt(right);
+             smap.put(ch,smap.getOrDefault(ch,0)+1);
+             if(pmap.containsKey(ch) && smap.get(ch)<=pmap.get(ch)) {
+                count--;
+             }
+             if(right-left+1 > p.length()) {
+                char leftchar = s.charAt(left);
+                if(pmap.containsKey(leftchar) && smap.get(leftchar)<=pmap.get(leftchar)) {
+                  count++;
+             }
+             smap.put(leftchar,smap.get(leftchar)-1);
+             left++;
+             }
+             if(count==0) {
+                res.add(left);
+             }
         }
         return res;
     }
